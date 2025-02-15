dabc72fa85ed8bedb4301fa3292098bc008249d0
brizzinck <87083857+brizzinck@users.noreply.github.com>
2025-02-15 14:43:26 +0200

    feat: resizable

--- src/plugins/default.rs
+++ src/plugins/default.rs
@@ -13,31 +13,57 @@ impl Plugin for RustydokuDefault {
             ..default()
         }));
 
+        app.add_systems(Startup, spawn_background);
         app.add_systems(Update, enforce_portrait_mode);
     }
 }
+
+fn spawn_background(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        Sprite {
+            image: asset_server.load("background.png"),
+            ..default()
+        },
+        Transform {
+            translation: Vec3::new(0.0, 0.0, -1.0),
+            scale: Vec3::splat(1.0),
+            ..Default::default()
+        },
+        Background,
+    ));
+}
+
+#[derive(Component)]
+struct Background;
 
 fn enforce_portrait_mode(
     mut resize_events: EventReader<WindowResized>,
     mut windows: Query<&mut bevy::window::Window>,
     mut cameras: Query<&mut OrthographicProjection, With<Camera2d>>,
+    mut backgrounds: Query<&mut Transform, With<Background>>,
 ) {
     for event in resize_events.read() {
         if let Ok(mut window) = windows.get_single_mut() {
             let (width, height) = (event.width, event.height);
 
-            if width > height {
-                window.resolution.set(height, width);
+            let (effective_width, effective_height) = if width > height {
+                (height, height)
             } else {
-                window.resolution.set(width, height);
-            }
+                (width, height)
+            };
+
+            window.resolution.set(effective_width, effective_height);
+
+            let scale_w = 420. / effective_width;
+            let scale_h = 645. / effective_height;
+            let required_scale = scale_w.max(scale_h);
 
             if let Ok(mut projection) = cameras.get_single_mut() {
-                let map_half_size = 160.0;
-
-                projection.scale = (map_half_size * 2.0) / height * 2.2;
-
+                projection.scale = required_scale;
                 info!("Updated camera scale: {}", projection.scale);
+                if let Ok(mut background_transform) = backgrounds.get_single_mut() {
+                    background_transform.scale = Vec3::new(1., 1., 1.0);
+                }
             }
         }
     }
