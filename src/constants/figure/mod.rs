0bf7458c468c8eb3709d21b724de7c69b212f384
Vlad Lavrishko <lavrishkovlad@gmail.com>
2025-02-25 12:41:39 +0200

    fix: add factor to upscaling each frame figure

--- src/constants/figure/mod.rs
+++ src/constants/figure/mod.rs
@@ -14,6 +14,7 @@ pub const FIGURE_POSITION_Z: f32 = 1.;
 pub const FIGURE_DRAGGING_SCALE: f32 = 1.;
 pub const FIGURE_IDEL_SCALE: f32 = 0.6;
 pub const FIGURE_SPEED_TO_UPSCALE: f32 = 8.;
+pub const FIGURE_UPSCALE_SPEED_INCREMENT_FACTOR_PER_FRAME: f32 = 1.;
 pub const FIGURE_OFFSET_DRAGGING_Y: f32 = 55.;
 pub const FIGURE_OFFSET_DRAGGING_Y_MULTIPLY: f32 = 10.;
 pub const INTERACTIVE_ZONE_COLOR: Srgba = Srgba::new(0., 0., 0., 0.);
--- src/logic/animation/figure.rs
+++ src/logic/animation/figure.rs
@@ -1,5 +1,8 @@
 use crate::{
-    constants::figure::{FIGURE_DRAGGING_SCALE, FIGURE_SPEED_TO_UPSCALE},
+    constants::figure::{
+        FIGURE_DRAGGING_SCALE, FIGURE_SPEED_TO_UPSCALE,
+        FIGURE_UPSCALE_SPEED_INCREMENT_FACTOR_PER_FRAME,
+    },
     states::figure::StateFigureAnimation,
 };
 use bevy::prelude::*;
@@ -7,7 +10,9 @@ use bevy::prelude::*;
 pub fn upscaling_dragging(transform: &mut Transform, delta: f32, state: &mut StateFigureAnimation) {
     transform.scale = transform.scale.lerp(
         Vec3::splat(FIGURE_DRAGGING_SCALE),
-        delta * FIGURE_SPEED_TO_UPSCALE,
+        delta
+            * FIGURE_SPEED_TO_UPSCALE
+            * (FIGURE_UPSCALE_SPEED_INCREMENT_FACTOR_PER_FRAME + transform.scale.x),
     );
 
     if transform.scale.x >= 0.99 {
