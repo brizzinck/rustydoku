fa8eb61f8863fc80b33a424c34ac3d5923a78902
Vlad Lavrishko <lavrishkovlad@gmail.com>
2025-03-06 22:57:15 +0200

    fix: figures

--- src/constants/figure/mod.rs
+++ src/constants/figure/mod.rs
@@ -7,18 +7,17 @@ use bevy::math::Vec2;
 pub use interactive::*;
 pub use transform::*;
 
-pub const FIGURES: [(&[Vec2], &str); 14] = [
+pub const FIGURES: [(&[Vec2], &str); 13] = [
     (&C, C_NAME),
     (&SQUARE, SQUARE_NAME),
     (&BIG_T, BIG_T_NAME),
     (&CUBE, CUBE_NAME),
-    (&HORIZONTAL_LINE_3, HORIZONTAL_LINE_3_NAME),
+    (&LINE_3, LINE_3_NAME),
     (&T, T_NAME),
     (&BIG_L, BIG_L_NAME),
     (&L, L_NAME),
-    (&HORIZONTAL_LINE_2, HORIZONTAL_LINE_2_NAME),
-    (&VERTICAL_LINE_3, VERTICAL_LINE_3_NAME),
-    (&VERTICAL_LINE_2, VERTICAL_LLINE_2_NAME),
+    (&LINE_2, LINE_2_NAME),
+    (&LINE_3, LINE_3_NAME),
     (&SMALL_L, SMALL_L_NAME),
     (&ZIG_ZAG, ZIG_ZAG_NAME),
     (&BIG_ZIG_ZAG, BIG_ZIG_ZAG_NAME),
@@ -68,17 +67,11 @@ const CUBE: [Vec2; 4] = [
 ];
 const CUBE_NAME: &str = "Cube";
 
-const HORIZONTAL_LINE_3: [Vec2; 3] = [Vec2::new(0., 0.), Vec2::new(1., 0.), Vec2::new(-1., 0.)];
-const HORIZONTAL_LINE_3_NAME: &str = "Horizontal Line 3";
+const LINE_3: [Vec2; 3] = [Vec2::new(0., 0.), Vec2::new(1., 0.), Vec2::new(-1., 0.)];
+const LINE_3_NAME: &str = "Horizontal Line 3";
 
-const HORIZONTAL_LINE_2: [Vec2; 2] = [Vec2::new(-0.5, 0.), Vec2::new(0.5, 0.)];
-const HORIZONTAL_LINE_2_NAME: &str = "Horizontal Line 2";
-
-const VERTICAL_LINE_3: [Vec2; 3] = [Vec2::new(0., 0.), Vec2::new(-1., -1.), Vec2::new(1., 1.)];
-const VERTICAL_LINE_3_NAME: &str = "Vertical Line 3";
-
-const VERTICAL_LINE_2: [Vec2; 2] = [Vec2::new(-0.5, -0.5), Vec2::new(0.5, 0.5)];
-const VERTICAL_LLINE_2_NAME: &str = "Vertical Line 2";
+const LINE_2: [Vec2; 2] = [Vec2::new(-0.5, 0.), Vec2::new(0.5, 0.)];
+const LINE_2_NAME: &str = "Horizontal Line 2";
 
 const BIG_ZIG_ZAG: [Vec2; 5] = [
     Vec2::new(-1., -1.),
