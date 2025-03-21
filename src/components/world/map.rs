f4ee1c6eb117aa995d629730d8eec55efa9a84db
Vlad Lavrishko <lavrishkovlad@gmail.com>
2025-03-22 01:10:42 +0200

    fix: tiles alpha

--- src/components/world/map.rs
+++ src/components/world/map.rs
@@ -31,6 +31,7 @@ impl TileComponent {
             Sprite {
                 custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
                 image: image.clone(),
+                color: Color::srgba(1., 1., 1., 0.75).into(),
                 ..default()
             },
             Transform::from_translation(position),
