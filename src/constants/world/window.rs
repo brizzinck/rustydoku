64c790c033d2b689c771e17c00d370b36572560d
Vlad Lavrishko <lavrishkovlad@gmail.com>
2025-03-23 04:18:48 +0200

    fix: window resolution and icon

--- src/constants/world/window.rs
+++ src/constants/world/window.rs
@@ -5,3 +5,6 @@ pub const WINDOW_WIDTH_SCALED_FACTOR: f32 = 420.;
 /// This value represents the designed height and is used as a reference
 /// when scaling content relative to different screen resolutions.
 pub const WINDOW_HEIGHT_SCALED_FACTOR: f32 = 645.;
+
+/// The icon used for the window.
+pub const WINDOW_ICON_PATH: &str = "assets/figure/ferris.png";
--- src/plugins/default.rs
+++ src/plugins/default.rs
@@ -1,32 +1,63 @@
-use crate::constants::world::window::{WINDOW_HEIGHT_SCALED_FACTOR, WINDOW_WIDTH_SCALED_FACTOR};
+use crate::constants::world::window::{
+    WINDOW_HEIGHT_SCALED_FACTOR, WINDOW_ICON_PATH, WINDOW_WIDTH_SCALED_FACTOR,
+};
 use bevy::log::LogPlugin;
 use bevy::prelude::*;
-use bevy::window::WindowPlugin;
+use bevy::window::{PrimaryWindow, WindowPlugin};
+use bevy::winit::WinitWindows;
+use winit::window::Icon;
 
 /// The default plugin window app for Rustydoku
 pub struct RustydokuDefaultPlugin;
 
 impl RustydokuDefaultPlugin {
     /// Fit the window to the viewport of all resolutions
+    ///
+    /// Parameters:
+    /// - `windows`: Query<Window> - The window query
+    /// - `cameras`: Query<OrthographicProjection> - The camera query
     fn fit_window_to_viewport(
         mut windows: Query<&mut bevy::window::Window>,
         mut cameras: Query<&mut OrthographicProjection, With<Camera2d>>,
     ) {
-        if let Ok(mut window) = windows.get_single_mut() {
+        if let Ok(window) = windows.get_single_mut() {
             let width = window.resolution.width();
             let height = window.resolution.height();
 
-            let (effective_width, effective_height) = if width > height {
-                (height, width)
-            } else {
-                (width, height)
-            };
-
-            window.resolution.set(effective_width, effective_height);
             if let Ok(mut projection) = cameras.get_single_mut() {
-                let scale_width = WINDOW_WIDTH_SCALED_FACTOR / effective_width;
-                let scale_height = WINDOW_HEIGHT_SCALED_FACTOR / effective_height;
+                let scale_width = WINDOW_WIDTH_SCALED_FACTOR / width;
+                let scale_height = WINDOW_HEIGHT_SCALED_FACTOR / height;
                 projection.scale = scale_width.max(scale_height);
+            }
+        }
+    }
+
+    // Set the window icon
+    //
+    // Parameters:
+    // - `windows`: NonSend<WinitWindows> - The windows query
+    // - `primary`: Query<Entity, With<PrimaryWindow>> - The primary window query
+    fn set_window_icon(
+        windows: NonSend<WinitWindows>,
+        primary: Query<Entity, With<PrimaryWindow>>,
+    ) {
+        if let Ok(primary_entity) = primary.get_single() {
+            if let Some(primary_window) = windows.get_window(primary_entity) {
+                let (icon_rgba, icon_width, icon_height) = {
+                    let image = image::open(WINDOW_ICON_PATH)
+                        .expect("Failed to open icon path")
+                        .into_rgba8();
+
+                    let (width, height) = image.dimensions();
+                    let rgba = image.into_raw();
+
+                    (rgba, width, height)
+                };
+
+                let icon = Icon::from_rgba(icon_rgba, icon_width, icon_height)
+                    .expect("Failed to create icon");
+
+                primary_window.set_window_icon(Some(icon));
             }
         }
     }
@@ -50,6 +81,9 @@ impl Plugin for RustydokuDefaultPlugin {
                 .build()
                 .disable::<LogPlugin>(),
         );
+
+        trace!("Setting up set window icon system");
+        app.add_systems(Startup, Self::set_window_icon);
 
         trace!("Setting up fit window to viewport system");
         app.add_systems(Update, Self::fit_window_to_viewport);
